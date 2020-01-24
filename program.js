c1121808c18efef6bbbdc4d8e247df79aab492e2
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-24 15:07:21 +0530

    Form,Message url,Search are completed.

--- program.js
+++ program.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const process = require('process');
+const path = require('path');
+const fs = require('fs');
+const bodyparser = require('body-parser');
 
 const app = express();
 const PORT = process.argv[2];
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static('public'));
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+app.use(express.static(process.argv[3]));
 
 app.set('views', process.argv[3])
 app.set('view engine', 'pug');
@@ -18,6 +27,29 @@ app.get('/home', (req, res) => {
 app.post('/form', (req, res) => {
   console.log(req.body);
   res.send(req.body.str.split('').reverse().join(''))
+});
+
+app.put('/message/:id', (req, res, next) => {
+  const id = req.params.id;
+  const str = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  next();
+  res.send(str)
+});
+
+app.get('/search', (req, res) => {
+  const query = req.query;
+  res.send(query);
+});
+
+app.get('/books', (req, res) => {
+  fs.readFile(process.argv[3], (err, data) => {
+    if (err) throw err;
+    const books = JSON.parse(data.toString());
+    res.json(books);
+  })
 });
 
 app.listen(PORT);
