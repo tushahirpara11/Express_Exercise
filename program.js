b2e7149cb8c72e7e1f547b9b1b5870b5d3c619a3
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-24 14:56:19 +0530

    use middleware and static method are completed

--- program.js
+++ program.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.argv[2];
+
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.end('Hello World!')
