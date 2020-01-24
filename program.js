d7d9fac7b6749e8b306bf470d6b95ff7ee2a3a4a
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-24 14:53:55 +0530

    simple routes is completed.

--- program.js
+++ program.js
@@ -1,19 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const PORT = process.argv[2];
 
-const port = process.argv[2];
-
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
-
-app.get('/home', (req, res) => {
-  res.end("Hello World!");
+app.get('/', function (req, res) {
+  res.end('Hello World!')
 });
-
-app.set('views', process.argv[3]);
-app.set('view engine', 'pug');
-app.get('/home', (req, res) => {
-  res.render('index', { date: new Date().toDateString() });
-});
-
-app.listen(port);
+app.listen(PORT);
