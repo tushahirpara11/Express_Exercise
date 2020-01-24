ac7050684aa85b249dbf3115f3b8274fb75fe1d2
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-01-24 15:04:13 +0530

    pug is completed.

--- program.js
+++ program.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const path = require('path');
+const process = require('process');
+
 const app = express();
 const PORT = process.argv[2];
 
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.set('views', process.argv[3])
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.end('Hello World!')
 });
+
+app.get('/home', (req, res) => {
+  res.render('index', { date: new Date().toDateString() });
+});
+
+app.post('/form', (req, res) => {
+  console.log(req.body);
+  res.send(req.body.str.split('').reverse().join(''))
+});
+
 app.listen(PORT);
